a8153b797a7501563f0d74c2ba7eeaa16016ba46
TheBull15 <82070933+TheBull15@users.noreply.github.com>
2021-04-09 16:39:52 -0400

    6

--- index.js
+++ index.js
@@ -166,6 +166,24 @@ client.on('message', message => {
                 message.channel.send("You can't use this command!")
             }
             break;
+        case 'ban':
+
+            const user = message.mentions.users.first();
+
+            if (user) {
+                const member = message.guild.member(user);
+
+                if (member) {
+                    member.ban({ ression: 'You were bad!' }).then(() => {
+                        message.reply(`${user.tag} has been banned!`)
+                    })
+                } else {
+                    message.reply("That user isn't in this guild!")
+                }
+            } else {
+                message.reply('You need specify a person!')
+            }
+            break;
         case 'fact':
             if (message.member.roles.cache.has('798965111687217192')) {
                 rndmessage(message);
