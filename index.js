4aab72780d831be0b8c22a041a634b2b558b96fd
TheBull15 <82070933+TheBull15@users.noreply.github.com>
2021-04-11 17:45:21 -0400

    unmute

--- index.js
+++ index.js
@@ -118,6 +118,21 @@ client.on('message', message => {
             } else {
                 message.channel.send("You can't use this command")
             }
+        case 'unmute':
+            if (message.member.roles.cache.has('798965109950120016')) {
+                let personmuted = message.guild.member(message.mentions.users.first())
+                if (!personmuted) return message.reply("error, member not found!");
+
+                let mainrole1 = message.guild.roles.cache.find(role => role.name === "Sexy Member");
+                let muterole1 = message.guild.roles.cache.find(role => role.name === "Muted");
+
+                personmuted.roles.add(mainrole1.id);
+                personmuted.roles.remove(muterole1.id);
+                message.channel.bulkDelete(1);
+                message.channel.send(`@${personmuted.user.tag} has been unmuted!`)
+            } else {
+                message.channel.send("You can't use this command!")
+            }
         case 'kick':
             if (message.member.roles.cache.has('798983712149733397')) {
 
