3f997935d171a363a97cdfb9cafb69d3fb7c4f71
TheBull15 <82070933+TheBull15@users.noreply.github.com>
2021-04-11 01:16:50 -0400

    Fixed the anthem command.

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ client.on('message', message => {
             if (message.member.roles.cache.has('798965111687217192')) {
                 const embed = new Discord.MessageEmbed()
                     .setTitle('Anthem')
-                    .addFeild("https://www.youtube.com/watch?v=wyx6JDQCslE")
+                    .setURL("https://www.youtube.com/watch?v=wyx6JDQCslE")
                     .setColor(0xff0000)
                     .setFooter('Stay Sexy!')
             } else {
